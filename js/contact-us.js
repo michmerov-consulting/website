5f3abec52647826226f21aaa4c34c7610959d38b
Bryan Michmerhuizen <bryan@michmerov.consulting>
2020-06-11 15:56:17 -0400

    new submit button processing

--- js/contact-us.js
+++ js/contact-us.js
@@ -4,6 +4,20 @@
 {
 	var submitButtonLabel = document.getElementById("submit-button-label");
 	var submitButton = document.getElementById("submit-button");
+	document.body.addEventListener("submit", async function(event) {
+        event.preventDefault();
+        
+        const form = event.target;
+		const data = new URLSearchParams(new FormData(formElement));
+		const result = await fetch(form.action, {
+          method: form.method,
+          body: data
+        })        
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch(error => console.log(error)); 
+		form.innerHtml=result;
+      });
 	function recaptchaReadyOnLoad(){
 		return true;
 	}
